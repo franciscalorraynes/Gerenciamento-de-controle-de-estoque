5df593c8bf63970e373e2d7d1e391f6fb6173f3c
franciscalorraynes <franciscalorrayne5@gmail.com>
2023-10-02 11:55:43 -0300

    commit initial

---
+++ InterfaceProduto.java
@@ -0,0 +1,20 @@
+public interface InterfaceProduto{
+    public void cadastraProduto();
+    public void imprime_lista();
+    
+
+}
+
+
+// Produtoss *cadastraProduto(void);
+// void imprimeProduto(Produtoss *prod);
+// Listaproduto *inicializar(void);
+// void imprime_lista(Listaproduto *l);
+// void listar(Listaproduto *lista);
+// Listaproduto *busca(int elemento, Listaproduto *l);
+// Listaproduto *excluir(Listaproduto *l, int qtde_estoque);
+// Listaproduto *adciona_produto_departamento(Listaproduto *ListaProduto, Produtoss *prod);
+// void salvar_arquivo(Listaproduto *lis);
+// void editar_produtos(Listaproduto *lista);
+// void liberaLista(Listaproduto *l);
+// #endif
---
+++ Produto.java
@@ -0,0 +1,37 @@
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class Produto {
+    int codigoProduto;
+    double precoProduto;
+    Date dataFabricacao;
+    String tipoProduto;
+    Date validadeDoProduto;
+    
+    public Produto(int codigoProduto, float precoProduto, Date dataFabricacao, String tipoProduto, Date validadeDoProduto) {
+        this.codigoProduto = codigoProduto;
+        this.precoProduto = precoProduto;
+        this.dataFabricacao = dataFabricacao;
+        this.tipoProduto = tipoProduto;
+        this.validadeDoProduto = validadeDoProduto;
+    }
+
+    public void cadastraProduto(){
+        SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o Código do produto: ");
+        int cod = entrada.nextInt();
+        System.out.println("");
+        double preco = entrada.nextDouble();
+        System.out.println("Data de fabricação: ");
+        Date data = entrada.
+        System.out.println("Digite o tipo do produto: ");
+        String tipoProd = entrada.nextLine();
+        System.out.println("");
+        Date validade = entrada.nextLine();
+
+    }
+
+    
+}
